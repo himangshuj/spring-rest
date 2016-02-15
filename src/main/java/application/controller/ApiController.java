568cbc6bf2b0339b589f412ae7d92021d60369ec
himangshu <himangshu@gozoomo.com>
2016-02-15 17:22:05 +0530

    Http forwarding working
    Signed-off-by: himangshu <himangshu@gozoomo.com>

---
+++ src/main/java/application/controller/ApiController.java
@@ -0,0 +1,7 @@
+package application.controller;
+
+/**
+ * Created by himangshu on 15/2/16.
+ */
+public class ApiController {
+}
---
+++ src/main/java/application/controller/MvcConfig.java
@@ -0,0 +1,27 @@
+package application.controller;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by himangshu on 15/2/16.
+ */
+
+@Configuration
+@ComponentScan
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:templates/");
+    }
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
+
+}
